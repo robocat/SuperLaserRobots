3cdb83cbe3f23c6e4931c3c6e685b53e4f9b8efa
Kristian Andersen <hello@kristian.co>
2015-02-06 23:31:08 +0100

    Added projectile

--- MirrorLaserTag/GameScene.swift
+++ MirrorLaserTag/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    
     override func didMoveToView(view: SKView) {
         
     }
---
+++ MirrorLaserTag/Projectile.swift
@@ -0,0 +1,13 @@
+//
+//  Projectile.swift
+//  MirrorLaserTag
+//
+//  Created by Kristian Andersen on 06/02/15.
+//  Copyright (c) 2015 Robocat. All rights reserved.
+//
+
+import SriteKit
+
+class Projectile: SKSpriteNode {
+    
+}
