8c1f3702328d42a6cf4ef4f4305192e067879291
Kristian Andersen <hello@kristian.co>
2015-02-07 00:04:43 +0100

    Added bullet

--- MirrorLaserTag/Projectile.swift
+++ MirrorLaserTag/Projectile.swift
@@ -9,6 +9,21 @@
 import SpriteKit
 
 class Projectile: SKSpriteNode {
+	
+	convenience init(position: CGPoint, angle: Double) {
+		let texture = SKTexture(imageNamed: "bullet")
+		self.init(texture: texture, color: nil, size: texture.size())
+	}
+	
+	override init(texture: SKTexture!, color: NSColor!, size: CGSize) {
+		super.init(texture: texture, color: color, size: size)
+		setupPhysics()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+	    fatalError("init(coder:) has not been implemented")
+	}
+	
 	func setupPhysics() {
 		physicsBody = SKPhysicsBody(texture: texture, size: size)
 		physicsBody?.affectedByGravity = false
