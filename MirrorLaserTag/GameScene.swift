57b584679ba32fce34eb0906a0f61eefb19696d7
Kristian Andersen <hello@kristian.co>
2015-02-08 13:08:06 +0100

    Health is less frequent dropped. Heals 30

--- MirrorLaserTag/GameScene.swift
+++ MirrorLaserTag/GameScene.swift
@@ -52,7 +52,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PlayerDelegate {
 		addChild(countdown)
 		
 
-		let wait = SKAction.waitForDuration(10, withRange: 5)
+		let wait = SKAction.waitForDuration(20, withRange: 5)
 		let runBlock = SKAction.runBlock {
 			if self.waiting {
 				return
@@ -81,8 +81,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PlayerDelegate {
 		powerUp.position = randomPoints[Int(index)]
 		
 		map.addChild(powerUp)
-
-		startLobbyMusic()
     }
 	
 	func startLobbyMusic() {
@@ -358,7 +356,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PlayerDelegate {
 		switch collision {
 		case PhysicsType.PowerUp.rawValue | PhysicsType.Player.rawValue:
 			if let player = contact.bodyA.node as? Player {
-				player.health = min(player.health + 20, 100)
+				player.health = min(player.health + 30, 100)
 
 				contact.bodyB.node?.removeFromParent()
 			}
